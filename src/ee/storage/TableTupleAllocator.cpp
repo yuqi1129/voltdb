ab22a3fc3b2c90828b7c9a9169ccbeb64b94b051
voltdbci <test@voltdb.com>
2020-03-12 19:22:00 -0400

    done

--- src/ee/storage/TableTupleAllocator.cpp
+++ src/ee/storage/TableTupleAllocator.cpp
@@ -1877,10 +1877,11 @@ HookedCompactingChunks<Hook, E>::freeze() {
 }
 
 template<typename Hook, typename E>
-inline void HookedCompactingChunks<Hook, E>::thaw() {
+template<typename Tag> inline void HookedCompactingChunks<Hook, E>::thaw() {
     Hook::thaw();
     CompactingChunks::thaw();
     m_observerable = false;
+    reinterpret_cast<observer_type<Tag>&>(m_iterator_observer).reset();
 }
 
 template<typename Hook, typename E> inline void HookedCompactingChunks<Hook, E>::remove_add(void* p) {
@@ -2110,7 +2111,7 @@ template void TxnPreHook<alloc, HistoryRetainTrait<gc>>::add<typename           
 template shared_ptr<typename IterableTableTupleChunks<                                   \
         HookedCompactingChunks<TxnPreHook<alloc, HistoryRetainTrait<gc>>, void>, tag, void>::hooked_iterator>    \
 HookedCompactingChunks<TxnPreHook<alloc, HistoryRetainTrait<gc>>, void>::freeze<tag>();  \
-template void HookedCompactingChunks<TxnPreHook<alloc, HistoryRetainTrait<gc>>, void>::thaw<tag>(void*);         \
+template void HookedCompactingChunks<TxnPreHook<alloc, HistoryRetainTrait<gc>>, void>::thaw<tag>();              \
 template void HookedCompactingChunks<TxnPreHook<alloc, HistoryRetainTrait<gc>>, void>::update<tag>(void*);       \
 template void HookedCompactingChunks<TxnPreHook<alloc, HistoryRetainTrait<gc>>, void>::clear<tag>();             \
 template void const* HookedCompactingChunks<TxnPreHook<alloc, HistoryRetainTrait<gc>>, void>::remove<tag>(void*);\
